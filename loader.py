e79ccf854233973c66011f11f34ec4ded0cf01b9
Filipe Cabecinhas <filcab+github@gmail.com>
2012-02-27 09:25:21 +0000

    rephrase comment

--- loader.py
+++ loader.py
@@ -1,7 +1,7 @@
 from lldb_plugin.sublime_lldb import *
 
 # If we only import those names, it works too. But we get lint errors.
-# So we import everything and export only these names.
+# So we import everything and export only the command names.
 
 # lldb
 # This command opens the input panel for the debugger.
