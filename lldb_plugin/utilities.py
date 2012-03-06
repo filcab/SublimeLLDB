f224e4eb272e59d6ab03b6976eb0832d1df2f227
Filipe Cabecinhas <filcab+github@gmail.com>
2012-03-06 19:58:10 +0000

    remove the last 'err> ' from stderr_msgs

--- lldb_plugin/utilities.py
+++ lldb_plugin/utilities.py
@@ -1,11 +1,18 @@
 # utilities for the sublime lldb plugin
 
-# import root_objects
+
+def debug(thing):
+    print thing
 
 
 def stderr_msg(string):
     if string is not None and len(string) > 0:
-        string = 'err> ' + string.replace('\n', '\nerr >')
+        string = 'err> ' + string.replace('\n', '\nerr> ')
+
+    if string[-6:] == '\nerr> ':
+        debug('newline at the end')
+        string = string[:-5]
+
     return string
 
 
