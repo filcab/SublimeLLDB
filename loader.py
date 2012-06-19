14927d135c92f43be0a5b1777c596932b5ba768b
Filipe Cabecinhas <filcab+github@gmail.com>
2012-06-19 15:51:27 +0100

    Remove old import

--- loader.py
+++ loader.py
@@ -1,5 +1,4 @@
 from lldb_plugin.sublime_lldb import *
-from lldb_plugin.monitors import MarkersListener
 
 # If we only import those names, it works too. But we get lint errors.
 # So we import everything and export only the command names.
