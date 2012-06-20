418dd4f7e9db5f9b37135e20e2d4286342d1eab2
Filipe Cabecinhas <filcab+github@gmail.com>
2012-06-20 13:26:00 +0100

    Added debug.py

---
+++ lldb_plugin/debug.py
@@ -0,0 +1,37 @@
+import sys
+import threading
+
+DFILE = sys.__stderr__
+_active = 0
+
+debugVerbose = 1 << 0
+debugViews = 1 << 1
+debugMonitors = 1 << 2
+debugLLDB = 1 << 3
+debugDriver = 1 << 4
+debugRoot = 1 << 5
+debugPlugin = 1 << 6
+debugSettings = 1 << 7
+
+debugAll = 0xff
+
+
+def debug(level, thing):
+    if _active & level == level:
+        print >> DFILE, threading.current_thread().name, str(thing)
+
+def toggle_debug(level):
+    global _active
+    _active = _active ^ level
+
+def set_debug(level):
+    global _active
+    _active = _active | level
+
+def unset_debug(level):
+    global _active
+    _active = _active & (~ level)
+
+def clear_debug():
+    global _active
+    _active = 0
