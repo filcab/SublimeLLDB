8bdc2011bd5462c84b52091ec27bff6604c5f7d1
Filipe Cabecinhas <filcab+github@gmail.com>
2012-06-25 16:50:01 +0100

    Updated LLDB.framework (2012-06-25)

--- lldb_plugin/LLDB.framework/Versions/A/Headers/lldb-defines.h
+++ lldb_plugin/LLDB.framework/Versions/A/Headers/lldb-defines.h
@@ -25,7 +25,7 @@
 //
 // A build script phase can modify this version number if needed.
 //----------------------------------------------------------------------
-#define LLDB_VERSION 152 
+#define LLDB_VERSION 160 
 
 //----------------------------------------------------------------------
 // LLDB defines
