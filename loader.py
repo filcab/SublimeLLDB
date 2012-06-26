fa638d05e69bc1aff14c6751d9485e674bdf52af
Filipe Cabecinhas <filcab+github@gmail.com>
2012-06-26 10:44:26 +0100

    Rename main plugin package

--- loader.py
+++ loader.py
@@ -1,5 +1,5 @@
-from lldb_plugin.sublime_lldb import *
-from lldb_plugin import monitors
+from plugin.sublime_lldb import *
+from plugin import monitors
 
 # If we only import those names, it works too. But we get lint errors.
 # So we import everything and export only the command names.
